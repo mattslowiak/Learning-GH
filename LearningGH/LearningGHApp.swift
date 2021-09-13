39dfc7b4b2548f1637781847314202e4f7796242
Matt Slowiak <gnipp@hotmail.com>
2021-09-12 19:03:54 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Matt Slowiak on 9/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
